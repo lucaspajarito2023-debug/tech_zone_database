b47faa34df540a4acd67a595bd269da0d461aa3c
lucaspajarito2023-debug <lucaspajarito2023@gmail.com>
2026-09-02 08:24:47 -0600

    feat: agregar datos a las tablas

--- dml/inserts.sql
+++ dml/inserts.sql
@@ -1,5 +1,5 @@
 
-INSERT  (nombre, apellido, telefono , emai) INTO clientes
+INSERT INTO clientes (nombre, apellido, telefono , email) 
     VALUES
 ('cliente 1', 'apellido 1', '12843034', 'cliente1@gmial.com'),
 ('cliente 2', 'apellido 2', '12843034', 'cliente1@gmial.com'),
@@ -18,3 +18,57 @@ INSERT  (nombre, apellido, telefono , emai) INTO clientes
 ('cliente 15', 'apellido 15', '12843034', 'cliente1@gmial.com'),
 ('cliente 16', 'apellido 16', '12843034', 'cliente1@gmial.com'),
 ('cliente 17', 'apellido 17', '12843034', 'cliente1@gmial.com');
+
+INSERT INTO proveedor (proveedor) VALUES
+    ('PROVEEDOR 1'),
+('PROVEEDOR 2'),
+('PROVEEDOR 3'),
+('PROVEEDOR 4');
+
+INSERT INTO categorias_p (categoria)
+    VALUES
+('Categoria 1'),
+('Categoria 2'),
+('Categoria 3'),
+    ('Categoria 4');
+
+INSERT INTO productos_tech(nombre_producto,
+    precio,
+    stock_disponible,
+    id_categoria,
+    id_proveedor) VALUES
+('PRODUCTO 1', 12.00, 56, 1, 2),
+('PRODUCTO 12', 12.00, 56, 1, 2),
+('PRODUCTO 14', 17.00, 56, 1, 2),
+('PRODUCTO 1212', 12.00, 66, 1, 2),
+('PRODUCTO 134', 12.00, 56, 1, 2),
+('PRODUCTO 15', 123.00, 56, 1, 2),
+('PRODUCTO 17', 1352.00, 56, 1, 2),
+('PRODUCTO 120', 123.00, 56, 1, 2),
+('PRODUCTO 123', 12.00, 56, 1, 2),
+('PRODUCTO 1453', 12.00, 56, 1, 2);
+
+INSERT INTO ventas_tech (id_cliente ,
+    cantidad,
+    id_producto )
+VALUES 
+(1, 45, 6),
+(1, 45, 6),
+(1, 45, 6),
+(1, 45, 6),
+(1, 45, 6),
+(1, 45, 6),
+(1, 45, 6);
+
+INSERT INTO detalles_venta (id_venta,
+    id_producto,
+    id_total,
+    fecha)
+VALUES 
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01'),
+(1, 6, 234.30, '2002-01-01');
